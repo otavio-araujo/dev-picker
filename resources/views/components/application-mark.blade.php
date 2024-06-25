4d2529eb7249d9c3ceb30ae8c6e2fd2ac67d22a4
otavio-araujo <otavio_araujo@hotmail.com>
2024-06-24 22:57:32 -0300

    FIX: changed Vite::asset for assets

--- resources/views/components/application-mark.blade.php
+++ resources/views/components/application-mark.blade.php
@@ -1,3 +1,3 @@
 <img {{ $attributes->merge(['class' => 'w-12 h-12 rounded-full']) }} alt="Beer and Code Logo"
-    src="{{ Vite::asset('storage/app/public/images/beer-and-code.png') }}">
+    src="{{ asset('images/beer-and-code.png') }}">
 <samp></samp>
